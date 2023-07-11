5df8c96e771f003e01fdbfc75191b2be5935740e
xshahbazintel <xshahbazintel@gmail.com>
2023-07-11 17:19:07 +0530

    Update test.py

--- .github/workflows/scripts/test.py
+++ .github/workflows/scripts/test.py
@@ -1,1 +1,21 @@
-print("This is a test")
+import argparse
+import json
+import os
+
+
+# parser = argparse.ArgumentParser(
+#     description="Publish Github event to RabbitMq Messanger")
+# github_event = parser.add_argument('--event',
+#                                    help='Provide Github event',
+#                                    required=True)
+# args = parser.parse_args()
+
+
+def validate_workflow_json():
+    # data = json.loads(args.github_event)
+    # print(type(data))
+    print(os.environ)
+
+
+if __name__ == "__main__":
+    validate_workflow_json()
